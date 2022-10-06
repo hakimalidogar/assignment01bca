b97caad7b3de44cebf8f9199c80f006e0e8b7e66
hakimalidogar <i192032@nu.edu.pk>
2022-10-06 20:38:38 +0500

    Initial commit

--- Assignment01bca_i192032.go
+++ Assignment01bca_i192032.go
@@ -1,4 +1,4 @@
-package assignment01bca
+package main
 
 import (
 	"crypto/sha256"
@@ -97,4 +97,67 @@ func VerifyChain(obj *Blockchain) bool {
 
 func CalculateHash(stringToHash string) string {
 	return fmt.Sprintf("%x", sha256.Sum256([]byte(stringToHash)))
+}
+
+func main() {
+	b1 := new(Blockchain)
+
+	var option int
+
+	for i := 0; true; i++ {
+		fmt.Printf("Press '1' to Add Block\n")
+		fmt.Printf("Press '2' to change Block\n")
+		fmt.Printf("Press '3' to VerifyBlockchain Block\n")
+		fmt.Printf("Press '4' to Display Blockchain Blocks\n")
+		fmt.Printf("Press '0' to Exit\n")
+		fmt.Printf("Select your option:\n")
+		fmt.Scan(&option)
+
+		if option == 1 {
+			var transaction [3]string
+			var nonce = 0
+
+			fmt.Printf("Enter transaction in fortmat: 'sender_to_reciever'\n\n");
+			fmt.Printf("Enter the 1 transaction: ");
+			fmt.Scan(&transaction[0])
+			fmt.Printf("\nEnter the 2 transaction: ");
+			fmt.Scan(&transaction[1])
+			fmt.Printf("\nEnter the 3 transaction: ");
+			fmt.Scan(&transaction[2])
+
+			fmt.Printf("\nEnter the nonce : ");
+			fmt.Scan(&nonce)
+
+			b1.Addblocktolist(transaction, nonce)
+			fmt.Printf("Block Added\n\n");
+		}else if option == 2 {
+			var transaction  = [3]string{}
+			var nonce = 0
+
+			fmt.Printf("Enter transaction in fortmat: 'sender_to_reciever'\n\n");
+			fmt.Printf("Enter the 1 new transaction: ");
+			fmt.Scan(&transaction[0])
+			fmt.Printf("\nEnter the 2 new transaction: ");
+			fmt.Scan(&transaction[1])
+			fmt.Printf("\nEnter the 3 new transaction: ");
+			fmt.Scan(&transaction[2])
+
+			fmt.Printf("\nEnter the nonce of desired block: ");
+			fmt.Scan(&nonce)
+
+			ChangeBlock(b1,nonce,transaction)
+			fmt.Printf("Block changed\n\n");
+		}else if option == 3 {
+			fmt.Printf("\n\n")
+			VerifyChain(b1)
+		}else if option == 4 {
+			fmt.Printf("\n\n")
+			ListBlocks(b1)
+		}else if option == 0 {
+			break
+		}
+	}
+
+	fmt.Printf("Exited\n\n")
+	
 }
